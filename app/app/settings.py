3281666690ec6e5aa8a1b3737db1e46ec4010d4c
pythoncrazy2 <ecpinpal@gmail.com>
2021-11-05 22:52:27 -0700

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0jypgiomp%f*vul1zuu$%hj+#o+to%jz-u_p)4i=_y2%0n=$tt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
