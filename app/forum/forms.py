6fcc7071370c04aed2dc5619d3f291ff4321f3fd
pythoncrazy2 <ecpinpal@gmail.com>
2021-11-06 12:22:37 -0700

    what the shit happened here

--- app/forum/forms.py
+++ app/forum/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 
 from .models import Comment, Topic
@@ -15,22 +14,4 @@ class CreateCommentForm(forms.ModelForm):
         model = Comment
         fields = [
             'body', 'author', 'post'
-=======
-from django import forms
-
-from .models import Comment, Topic
-
-class CreateTopicForm(forms.ModelForm):
-    class Meta:
-        model = Topic
-        fields = [
-            'title', 'description'
-        ]
-
-class CreateCommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = [
-            'body', 'author', 'post'
->>>>>>> 0f2e3bf08cb23908bad5d4911f58b073ee1a305b
         ]
