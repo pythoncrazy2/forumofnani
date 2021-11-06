27c91a15098827dcc0c3bb00a970c8f0b76d38c8
pythoncrazy2 <ecpinpal@gmail.com>
2021-11-06 12:38:05 -0700

    fixed procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ./app/wsgi.wsgi
+worker: python ./app/manage.py runserver
--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0jypgiomp%f*vul1zuu$%hj+#o+to%jz-u_p)4i=_y2%0n=$tt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
