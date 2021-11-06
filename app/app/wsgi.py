9155aecdd44d2459ee72e97999f1348498628c9c
pythoncrazy2 <ecpinpal@gmail.com>
2021-11-05 22:49:18 -0700

    .

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -10,14 +10,8 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
